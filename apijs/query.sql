48dd19eb626f6615f641b7660d39b9c2534bb916
derrianson <46056143+derrianson@users.noreply.github.com>
2022-11-02 20:53:59 +0530

    Delete apijs directory

--- apijs/query.sql
+++
@@ -1,9 +0,0 @@
--- select distinct(cityname) from hospital;
--- -- store city name
--- select hospitalname,hospitalid from hospital where cityname = 'Meaux';
--- -- store hospital id
--- select wardtype,wardid from ward where hospitalid = '69479325';
--- -- store wardid
--- select bedid,isAvailable,isprivate from bed where wardid = '38060243';
-
-
